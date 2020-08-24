63d4337fd86d3e0485aef9d79dbc1b00d3eba5a3
appooqqlips <70130855+appooqqlips@users.noreply.github.com>
2020-08-24 14:00:09 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,3 +2,7 @@ function myFunction() {
  document.getElementById("demo").innerHTML = "Paragraph changed.";
  console.log("This is console log");
 }
+function myFunction2() {
+ document.getElementById("demo").innerHTML = "Paragraph changed back.";
+ console.log("This is console log");
+}
